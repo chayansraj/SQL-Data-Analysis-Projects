ba7d01f4395a228652adca6e2e799fe1d4f31618
Chayan Shrang Raj <chayansraj95@gmail.com>
2023-10-06 10:28:29 +0200

    Create covid_death_usable_data.sql

---
+++ Project 1 - Covid Data Analysis/covid_death_usable_data.sql
@@ -0,0 +1,7 @@
+-- Select data that we will use
+
+SELECT location, date, total_cases, new_cases, total_deaths, population
+FROM `sqlcovidanalysis.covid_data_analysis.covid_deaths`
+ORDER BY location, date, population 
+
+-- It is clearly seen that deaths come after there are new cases so it makes sense.
