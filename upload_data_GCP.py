58f4c54bcccf5cdbe97178764e045ce0f677882d
CHAYAN SHRANG RAJ <chayansraj95@gmail.com>
2023-10-07 12:13:02 +0200

    add upload script

--- upload_data_GCP.py
+++ upload_data_GCP.py
@@ -4,8 +4,9 @@ import gcsfs
 from google.cloud import storage
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "D:\Downloads\sqlcovidanalysis-5149e3b8b26a.json"
-data = pd.read_excel('Nashville Housing Data.xlsx')
+#path = r"SQL-Data-Analysis-Projects\Project 2 - SQL Data Cleaning\Nashville Housing Data.xlsx"
+data = pd.read_excel("C:\\Users\\chaya\\Desktop\\SQL Data Analysis Projects\\SQL-Data-Analysis-Projects\\Project 2 - SQL Data Cleaning\\Nashville Housing Data.xlsx")
 
 client = storage.Client()
 bucket= client.get_bucket("nashville_raw")
-bucket.blob('nashville_raw.csv').upload_from_string(data.to_csv(index=False, sep=','), 'text/csv')
+bucket.blob('nashville_raw.csv').upload_from_string(data.to_csv(), 'text/csv')
