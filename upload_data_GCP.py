b5d6b97951a07ef0f30bf85b5986a3025f695238
Chayan Shrang Raj <chayansraj95@gmail.com>
2023-10-09 19:43:20 +0200

    Update upload_data_GCP.py

--- upload_data_GCP.py
+++ upload_data_GCP.py
@@ -7,6 +7,7 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "<your service account credential
 #path = r"SQL-Data-Analysis-Projects\Project 2 - SQL Data Cleaning\Nashville Housing Data.xlsx"
 data = pd.read_excel("C:\\Users\\chaya\\Desktop\\SQL Data Analysis Projects\\SQL-Data-Analysis-Projects\\Project 2 - SQL Data Cleaning\\Nashville Housing Data.xlsx")
 
+
 client = storage.Client()
 bucket= client.get_bucket("nashville_raw")
 bucket.blob('nashville_raw.csv').upload_from_string(data.to_csv(), 'text/csv')
