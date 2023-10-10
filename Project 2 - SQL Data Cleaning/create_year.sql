55eb6873b6d3f9e60412a3d602bb6069411e8d8b
Chayan Shrang Raj <chayansraj95@gmail.com>
2023-10-10 21:12:18 +0200

    Delete Project 2 - SQL Data Cleaning/create_year.sql

--- Project 2 - SQL Data Cleaning/create_year.sql
+++
@@ -1,9 +0,0 @@
-alter table `sqlcovidanalysis.sqldatacleaning.nashville_raw`
-drop column if exists sale_year; 
-
-alter table `sqlcovidanalysis.sqldatacleaning.nashville_raw`
-add column sale_year string;
-
-update `sqlcovidanalysis.sqldatacleaning.nashville_raw`
-set sale_year = (cast(EXTRACT (YEAR  FROM SaleDate ) as string))
-where true
