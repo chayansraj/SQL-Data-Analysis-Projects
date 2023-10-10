5e256950b4b1c0bcc5b8395c27ef10c8a3b5c1f8
Chayan Shrang Raj <chayansraj95@gmail.com>
2023-10-10 21:12:04 +0200

    Delete Project 2 - SQL Data Cleaning/create_date.sql

--- Project 2 - SQL Data Cleaning/create_date.sql
+++
@@ -1,9 +0,0 @@
-alter table `sqlcovidanalysis.sqldatacleaning.nashville_raw`
-drop column if exists sale_date; 
-
-alter table `sqlcovidanalysis.sqldatacleaning.nashville_raw`
-add column sale_date string;
-
-update `sqlcovidanalysis.sqldatacleaning.nashville_raw`
-set sale_date = (cast(EXTRACT (DAY  FROM SaleDate ) as string))
-where true
