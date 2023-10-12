42ac75116080f6e156d36d04293da33f12f041a1
Chayan Shrang Raj <chayansraj95@gmail.com>
2023-10-12 19:53:43 +0200

    Update standardize_date.sql

--- Project 2 - SQL Data Cleaning/standardize_date.sql
+++ Project 2 - SQL Data Cleaning/standardize_date.sql
@@ -1,15 +1,15 @@
-alter table `sqlcovidanalysis.sqldatacleaning.nashville_raw`
+alter table `sqlcovidanalysis.sqldatacleaning.nashville_raw`  -- a check to see if the field already exists
   drop column if exists sale_date
 , drop column if exists sale_year
 , drop column if exists sale_month;
 
-alter table `sqlcovidanalysis.sqldatacleaning.nashville_raw`
-add column sale_date string
+alter table `sqlcovidanalysis.sqldatacleaning.nashville_raw`  -- to add the column
+  add column sale_date string
 , add column sale_year string
 , add column sale_month string;
 
-update `sqlcovidanalysis.sqldatacleaning.nashville_raw`
+update `sqlcovidanalysis.sqldatacleaning.nashville_raw`  -- extract year, month and day from datetime field
   set sale_date = (cast(EXTRACT (DAY  FROM SaleDate ) as string))
 , sale_year = (cast(EXTRACT (YEAR  FROM SaleDate ) as string))  
 , sale_month = (cast(EXTRACT (MONTH  FROM SaleDate ) as string)) 
-where true;
+where true;  -- to affect all the rows in the table
